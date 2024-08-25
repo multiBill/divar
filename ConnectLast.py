062802e1ec2178ee52e7a67bf8017d65f587080b
multiBill <mohammadjalaldavooodian@gmail.com>
2024-08-25 12:04:38 +0330

    Update ConnectLast.py

--- ConnectLast.py
+++ ConnectLast.py
@@ -1,7 +1,9 @@
 #from requierment.txt import matplotlib
 #import os
 #os.system('pip install -r requirement.txt')
-py -m pip install "matplotlib==3.9.2"
+#py -m pip install "matplotlib==3.9.2"
+py -m pip install "matplotlib[PDF]==3.9.2"
+py -m pip install -e ".[PDF]" 
 import streamlit as st
 from BackendLast import columns,x_train,y_train,parameters_finder
 #from projectlas4 import df
